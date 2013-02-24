66135b1c5d97277d0e1db8204e4fbe76287b13dc
Alex Kizer <the.alex.kizer@gmail.com>
2013-02-24 10:43:36 -0600

    Initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankofCowderania2::Application.config.secret_token = '56148095c737e2df722b833fd441658fa854f1bb112f2ee88f4935d728dd494455e390c2773d3757d115af4f7972ee498cfd72298c3eaa3294596440ebd1ee79'
